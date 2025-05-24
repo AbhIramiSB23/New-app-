57ecde909979409e46f3dec5f511a7445cd77a5d
AbhIramiSB23 <abhiramisb2004@gmail.com>
2025-05-24 18:39:35 +0530

    Update main.jsx

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Navbar from './component/Navbar.jsx'
-import Add from './component/Add.jsx'
-import Home from './component/Home.jsx'
+import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <StrictMode>
     <App />
-    <Navbar></Navbar>
-    <Add></Add>
-    <Home></Home>
   </StrictMode>,
+  </BrowserRouter>
 )
